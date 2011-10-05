c24b07722c843e4447bf79c302ea2a18375bb934
Dennis Ploetner <re@lloc.de>
2011-10-05 14:30:55 +0200

    New class MslsCustomColumnTaxonomy

--- includes/MslsCustomColumn.php
+++ includes/MslsCustomColumn.php
@@ -12,24 +12,21 @@ require_once dirname( __FILE__ ) . '/MslsLink.php';
 
 class MslsCustomColumn extends MslsMain implements IMslsMain {
 
-    protected $post_type = 'post';
+    protected $type = 'post';
 
     static function init() {
         $options = MslsOptions::instance();
         if ( !$options->is_excluded() ) {
             $obj = new self();
-            if ( isset( $_REQUEST['taxonomy'] ) ) {
-                $obj->post_type = esc_attr( $_REQUEST['taxonomy'] );
-                add_filter( 'manage_edit-{$obj->taxonomy}_columns' , array( $obj, 'manage' ) );
-                add_action( 'manage_{$obj->taxonomy}_custom_column' , array( $obj, 'tx_columns' ), 10, 3 );
-            }
-            else {
-                if ( isset( $_REQUEST['post_type'] ) ) 
-                    $obj->post_type = esc_attr( $_REQUEST['post_type'] );
-                add_filter( 'manage_{$obj->post_type}s_posts_columns' , array( $obj, 'manage' ) );
-                add_action( 'manage_{$obj->post_type}s_custom_column' , array( $obj, 'pt_columns' ), 10, 2 );
-            }
+            if ( isset( $_REQUEST['post_type'] ) ) 
+                $obj->set_type ( $_REQUEST['post_type'] );
+            add_filter( 'manage_{$obj->post_type}_posts_columns' , array( $obj, 'manage' ) );
+            add_action( 'manage_{$obj->post_type}_posts_custom_column' , array( $obj, 'columns' ), 10, 2 );
         }
+    }
+
+    public function set_type( $type ) {
+        $this->type = esc_attr( $type );
     }
 
     function manage( $columns ) {
@@ -38,7 +35,7 @@ class MslsCustomColumn extends MslsMain implements IMslsMain {
             $arr = array();
             foreach ( $blogs as $blog ) {
                 $language = $blog->get_language();
-                $icon     = new MslsAdminIcon( $this->post_type );
+                $icon     = new MslsAdminIcon( $this->type );
                 $icon->set_language( $language );
                 $icon->set_src( $this->get_flag_url( $language, true ) );
                 $arr[] = $icon->get_img();
@@ -48,11 +45,7 @@ class MslsCustomColumn extends MslsMain implements IMslsMain {
         return $columns;
     }
 
-    public function tx_columns( $deprecated, $column_name, $term_id ) {
-        $this->columns( $column_name, $term_id );
-    }
-
-    public function pt_columns( $column_name, $post_id ) {
+    public function columns( $column_name, $post_id ) {
         $this->columns( $column_name, $post_id );
     }
 
@@ -60,11 +53,11 @@ class MslsCustomColumn extends MslsMain implements IMslsMain {
         if ( 'mslscol' == $column_name ) {
             $blogs = $this->blogs->get();
             if ( $blogs ) {
-                $mydata = MslsOptionsFactory::create( $this->post_type, $item_id );
+                $mydata = MslsOptionsFactory::create( $this->type, $item_id );
                 foreach ( $blogs as $blog ) {
                     switch_to_blog( $blog->userblog_id );
                     $language  = $blog->get_language();
-                    $edit_link = MslsAdminIcon::create( $this->post_type );
+                    $edit_link = MslsAdminIcon::create( $this->type );
                     $edit_link->set_language( $language );
                     if ( $mydata->has_value( $language ) ) {
                         $edit_link->set_src( $this->get_url( 'images' ) . '/link_edit.png' );
@@ -78,6 +71,24 @@ class MslsCustomColumn extends MslsMain implements IMslsMain {
                 }
             }
         }
+    }
+
+}
+
+class MslsCustomColumnTaxonomy extends MslsCustomColumn {
+
+    static function init() {
+        $options = MslsOptions::instance();
+        if ( !$options->is_excluded() ) {
+            $obj = new self();
+            $obj->type = esc_attr( $_REQUEST['taxonomy'] );
+            add_filter( 'manage_{$obj->taxonomy}_custom_column' , array( $obj, 'manage' ) );
+            add_action( 'manage_edit-{$obj->taxonomy}_columns' , array( $obj, 'columns' ), 10, 3 );
+        }
+    }
+
+    public function columns( $deprecated, $column_name, $term_id ) {
+        $this->columns( $column_name, $term_id );
     }
 
 }
