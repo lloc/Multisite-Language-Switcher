013e80f44a912d2934450286ac3780ec76fe5085
realloc <realloc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-06 13:25:32 +0000

    Refactoring MslsCustomColumn.php
    git-svn-id: http://plugins.svn.wordpress.org/multisite-language-switcher/trunk@448077 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/MslsCustomColumn.php
+++ includes/MslsCustomColumn.php
@@ -12,44 +12,33 @@ require_once dirname( __FILE__ ) . '/MslsLink.php';
 
 class MslsCustomColumn extends MslsMain implements IMslsMain {
 
+    protected $post_type = 'post';
+
     static function init() {
         $options = MslsOptions::instance();
         if ( !$options->is_excluded() ) {
             $obj = new self();
-            add_filter( 'manage_pages_columns' , array( $obj, 'pages_manage' ) );
-            add_filter( 'manage_posts_columns' , array( $obj, 'posts_manage' ) );
-            add_filter( 'manage_edit-category_columns' , array( $obj, 'category_manage' ) );
-            add_filter( 'manage_edit-post_tag_columns' , array( $obj, 'post_tag_manage' ) );
-            add_action( 'manage_pages_custom_column' , array( $obj, 'pages_columns' ), 10, 2 );
-            add_action( 'manage_posts_custom_column' , array( $obj, 'posts_columns' ), 10, 2 );
-            add_action( 'manage_category_custom_column' , array( $obj, 'category_columns' ), 10, 3 );
-            add_action( 'manage_post_tag_custom_column' , array( $obj, 'post_tag_columns' ), 10, 3 );
+            if ( isset( $_REQUEST['taxonomy'] ) ) {
+                $obj->post_type = esc_attr( $_REQUEST['taxonomy'] );
+                add_filter( 'manage_edit-{$obj->taxonomy}_columns' , array( $obj, 'manage' ) );
+                add_action( 'manage_{$obj->taxonomy}_custom_column' , array( $obj, 'tx_columns' ), 10, 3 );
+            }
+            else {
+                if ( isset( $_REQUEST['post_type'] ) ) 
+                    $obj->post_type = esc_attr( $_REQUEST['post_type'] );
+                add_filter( 'manage_{$obj->post_type}_posts_columns' , array( $obj, 'manage' ) );
+                add_action( 'manage_{$obj->post_type}_custom_column' , array( $obj, 'pt_columns' ), 10, 2 );
+            }
         }
     }
 
-    public function pages_manage( $columns ) {
-        $this->manage( 'page', $columns );
-    }
-
-    public function posts_manage( $columns ) {
-        $this->manage( 'post', $columns );
-    }
-
-    public function category_manage( $columns ) {
-        $this->manage( 'category', $columns );
-    }
-
-    public function post_tag_manage( $columns ) {
-        $this->manage( 'post_tag', $columns );
-    }
-    
-    function manage( $type, $columns ) {
+    function manage( $columns ) {
         $blogs = $this->blogs->get();
         if ( $blogs ) {
             $arr = array();
             foreach ( $blogs as $blog ) {
                 $language = $blog->get_language();
-                $icon     = new MslsAdminIcon( $type );
+                $icon     = new MslsAdminIcon( $this->post_type );
                 $icon->set_language( $language );
                 $icon->set_src( $this->get_flag_url( $language, true ) );
                 $arr[] = $icon->get_img();
@@ -59,31 +48,23 @@ class MslsCustomColumn extends MslsMain implements IMslsMain {
         return $columns;
     }
 
-    public function pages_columns( $column_name, $post_id ) {
-        $this->columns( 'page', $column_name, $post_id );
+    public function tx_columns( $deprecated, $column_name, $term_id ) {
+        $this->columns( $column_name, $term_id );
     }
 
-    public function posts_columns( $column_name, $post_id ) {
-        $this->columns( 'post', $column_name, $post_id );
+    public function pt_columns( $column_name, $post_id ) {
+        $this->columns( $column_name, $post_id );
     }
 
-    public function category_columns( $deprecated, $column_name, $term_id ) {
-        $this->columns( 'category', $column_name, $term_id );
-    }
-
-    public function post_tag_columns( $deprecated, $column_name, $term_id ) {
-        $this->columns( 'post_tag', $column_name, $term_id );
-    }
-
-    protected function columns( $type, $column_name, $item_id ) {
+    protected function columns( $column_name, $item_id ) {
         if ( 'mslscol' == $column_name ) {
             $blogs = $this->blogs->get();
             if ( $blogs ) {
-                $mydata = MslsOptionsFactory::create( $type, $item_id );
+                $mydata = MslsOptionsFactory::create( $this->post_type, $item_id );
                 foreach ( $blogs as $blog ) {
                     switch_to_blog( $blog->userblog_id );
                     $language  = $blog->get_language();
-                    $edit_link = MslsAdminIcon::create( $type );
+                    $edit_link = MslsAdminIcon::create( $this->post_type );
                     $edit_link->set_language( $language );
                     if ( $mydata->has_value( $language ) ) {
                         $edit_link->set_src( $this->get_url( 'images' ) . '/link_edit.png' );
