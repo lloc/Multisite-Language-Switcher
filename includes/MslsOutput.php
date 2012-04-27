7f928523417828f373bb5dacf90328b75783de71
Dennis Ploetner <re@lloc.de>
2012-04-27 13:10:08 +0200

    Use of is_singular() for the content filter

--- includes/MslsOutput.php
+++ includes/MslsOutput.php
@@ -197,7 +197,7 @@ add_action( 'widgets_init', 'msls_widgets_init' );
  * @return string
  */ 
 function msls_content_filter( $content ) {
-    if ( is_single() || is_page() ) {
+    if ( is_singular() ) {
         $options = MslsOptions::instance();
         if ( $options->is_content_filter() ) {
             $obj   = new MslsOutput();
