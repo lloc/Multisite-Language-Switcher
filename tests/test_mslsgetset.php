ab02ff9a295ac86fd452536fd3ca062dc6cb4aa3
Dennis Ploetner <re@lloc.de>
2012-08-18 14:27:58 +0200

    MslsGetSet Test

---
+++ tests/test_mslsgetset.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Tests MslsGetSet
+ *
+ * @author Dennis Ploetner <re@lloc.de>
+ * @package WP_Document_Revisions
+ */
+class WP_Test_MslsGetSet extends WP_UnitTestCase {
+
+	/**
+	 * SetUp initial settings
+	 */
+    function setUp() {
+		parent::setUp();
+		wp_cache_flush();
+	}
+
+
+	/**
+	 * Break down for next test
+	 */
+	function tearDown() {
+		parent::tearDown();
+	}
+
+	/**
+	 * Verify the reset method
+	 */
+	function test_reset_method() {
+        $obj      = new MslsGetSet();
+        $obj->str = 'This is a string.';
+        $obj->reset();
+		$this->assertEquals( array(), $obj->get_arr(), 'An empty array was expected' );
+	}
+
+}
