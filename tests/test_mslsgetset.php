6e366dfe835d0533911c0977c14dab2584e9cf30
Dennis Ploetner <re@lloc.de>
2012-08-18 15:34:30 +0200

    Test config modified

--- tests/test_mslsgetset.php
+++ tests/test_mslsgetset.php
@@ -8,30 +8,29 @@
  */
 class WP_Test_MslsGetSet extends WP_UnitTestCase {
 
-	/**
-	 * SetUp initial settings
-	 */
+    /**
+     * SetUp initial settings
+     */
     function setUp() {
-		parent::setUp();
-		wp_cache_flush();
-	}
+        parent::setUp();
+        wp_cache_flush();
+    }
 
+    /**
+     * Break down for next test
+     */
+    function tearDown() {
+        parent::tearDown();
+    }
 
-	/**
-	 * Break down for next test
-	 */
-	function tearDown() {
-		parent::tearDown();
-	}
-
-	/**
-	 * Verify the reset method
-	 */
-	function test_reset_method() {
+    /**
+     * Verify the reset method
+     */
+    function test_reset_method() {
         $obj      = new MslsGetSet();
         $obj->str = 'This is a string.';
         $obj->reset();
-		$this->assertEquals( array(), $obj->get_arr(), 'An empty array was expected' );
-	}
+        $this->assertEquals( array(), $obj->get_arr(), 'An empty array was expected' );
+    }
 
 }
