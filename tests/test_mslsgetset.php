e258f803c5371d481260a48ae698a8cebc9317bb
Dennis Ploetner <re@lloc.de>
2012-11-13 11:58:28 +0100

    Tests should now running on Travic-CI

--- tests/test_mslsgetset.php
+++ tests/test_mslsgetset.php
@@ -8,12 +8,15 @@
  */
 class WP_Test_MslsGetSet extends WP_UnitTestCase {
 
+	var $my_obj;
+
     /**
      * SetUp initial settings
      */
     function setUp() {
         parent::setUp();
         wp_cache_flush();
+		$this->my_obj = new MslsGetSet();
     }
 
     /**
@@ -27,11 +30,10 @@ class WP_Test_MslsGetSet extends WP_UnitTestCase {
      * Verify the get_arr-method
      */
     function test_get_arr_method() {
-        $obj = new MslsGetSet();
-        $obj->temp = 'test';
-        $this->assertEquals( array( 'temp' => 'test' ), $obj->get_arr() , 'Unexpected result' );
-        $obj->reset();
-        $this->assertEmpty( $obj->get_arr() , 'An empty array was expected' );
+        $this->my_obj->reset();
+        $this->assertEmpty( $this->my_obj->get_arr(), 'An empty array was expected' );
+        $his->my_obj->temp = 'test';
+        $this->assertEquals( array( 'temp' => 'test' ), $this->my_obj->get_arr() , 'Unexpected result' );
     }
 
 }
