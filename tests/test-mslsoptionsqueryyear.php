b0f663fd6287766e963b717368eeaaa60f936e6b
lloc <re@lloc.de>
2014-07-25 11:43:28 +0200

    Tests ongoing

--- tests/test-mslsoptionsqueryyear.php
+++ tests/test-mslsoptionsqueryyear.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Tests for MslsOptionsQuery
+ * Tests for MslsOptionsQueryYear
  *
  * @author Dennis Ploetner <re@lloc.de>
  * @package Msls
  */
 
 /**
- * WP_Test_MslsOptionsQuery
+ * WP_Test_MslsOptionsQueryYear
  */
-class WP_Test_MslsOptionsQuery extends WP_UnitTestCase {
+class WP_Test_MslsOptionsQueryYear extends WP_UnitTestCase {
 
 	/**
 	 * SetUp initial settings
@@ -27,16 +27,17 @@ class WP_Test_MslsOptionsQuery extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Verify the static create-method
+	 * Verify the has_value-method
 	 */
-	function test_create_method() {
-		$this->assertNull( MslsOptionsPost::create() );
-		return new MslsOptionsQuery();
+	function test_has_value_method() {
+		$obj = new MslsOptionsQueryYear();
+		$this->assertInternalType( 'boolean', $obj->has_value( 'de_DE' ) );
+		return $obj;
 	}
 
 	/**
 	 * Verify the get_current_link-method
-	 * @depends test_create_method
+	 * @depends test_get_postlink_method
 	 */
 	function test_get_current_link_method( $obj ) {
 		$this->assertInternalType( 'string', $obj->get_current_link() );
