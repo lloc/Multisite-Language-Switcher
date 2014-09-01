9e8a0a35ffc953e53c74d50fb6a0fe7bb9001a5a
lloc <re@lloc.de>
2014-09-01 15:45:37 +0200

    Fix test: The 'blog'-slug-problem :/

--- tests/test-mslsoptionstaxtermcategory.php
+++ tests/test-mslsoptionstaxtermcategory.php
@@ -15,7 +15,7 @@ class WP_Test_MslsOptionsTaxTermCategory extends Msls_UnitTestCase {
 	 * Verify the check_url-method
 	 */
 	function test_check_url_method() {
-		$obj = new WP_Test_MslsOptionsTaxTermCategory( 0 );
+		$obj = new MslsOptionsTaxTermCategory( 0 );
 		$this->assertInternalType( 'string', $obj->check_url( '' ) );
 		return $obj;
 	}
