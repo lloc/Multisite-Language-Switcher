7bcc4294d2cbbd8050683a1f1528ff18a28239bf
Dennis Ploetner <re@lloc.de>
2018-10-08 11:57:23 +0200

    Version bumped to 2.0

--- MultisiteLanguageSwitcher.php
+++ MultisiteLanguageSwitcher.php
@@ -4,7 +4,7 @@
 Plugin Name: Multisite Language Switcher
 Plugin URI: http://msls.co/
 Description: A simple but powerful plugin that will help you to manage the relations of your contents in a multilingual multisite-installation.
-Version: 1.2
+Version: 2.0
 Author: Dennis Ploetner
 Author URI: http://lloc.de/
 Text Domain: multisite-language-switcher
@@ -37,7 +37,7 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
  * @author Dennis Ploetner <re@lloc.de>
  */
 if ( ! defined( 'MSLS_PLUGIN_VERSION' ) ) {
-	define( 'MSLS_PLUGIN_VERSION', '1.2' );
+	define( 'MSLS_PLUGIN_VERSION', '2.0' );
 
 	if ( ! defined( 'MSLS_PLUGIN_PATH' ) ) {
 		define( 'MSLS_PLUGIN_PATH', plugin_basename( __FILE__ ) );
