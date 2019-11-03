de7bf5647de081fa2d5c81e7cd19927c3db3ab7e
Dennis Ploetner <re@lloc.de>
2019-11-03 18:16:25 +0100

    Tests added

--- tests/test-mslsadminicon.php
+++ tests/test-mslsadminicon.php
@@ -7,48 +7,76 @@ use Brain\Monkey\Functions;
 
 class WP_Test_MslsAdminIcon extends Msls_UnitTestCase {
 
-	public static function get_post_type() {
-		return [
-			[ 'post', 'http://example.org/wp-admin/post-new.php' ],
-			[ 'page', 'http://example.org/wp-admin/post-new.php?post_type=page' ]
-		];
+	protected $admin_url = 'https://example.org/wp-admin/';
+
+	public function get_test( $post ) {
+		return ( new MslsAdminIcon( $post->post_type ) )
+			->set_path()
+			->set_language( 'de_DE' )
+			->set_src( '/dev/german_flag.png' )
+			->set_href( $post->ID );
 	}
 
-	/**
-	 * Verify the class
-	 *
-	 * @param string $post_type
-	 *
-	 * @dataProvider get_post_type
-	 */
-	function test_class( $post_type, $create_link ) {
-		Functions\when( 'get_current_blog_id' )->justReturn( 1 );
-		Functions\when( 'get_admin_url' )->justReturn( '' );
-		Functions\when( 'add_query_arg' )->returnArg();
+	public function get_post( $post_type, $id = 0 ) {
+		$post = \Mockery::mock( '\WP_Post' );
+		$post->ID = $id;
+		$post->post_type = $post_type;
+		$post->post_author = $this->get_user();
 
+		return $post;
+	}
+
+	public function get_user() {
 		$user = \Mockery::mock( '\WP_User' );
 		$user->ID   = 1;
 		$user->role = 'editor';
 
-		$post = \Mockery::mock( '\WP_Post' );
-		$post->ID = 2;
-		$post->post_type = $post_type;
-		$post->post_author = $user;
+		return $user;
+	}
 
-		$obj = new MslsAdminIcon( $post->post_type );
+	public static function get_post_type() {
+		return [
+			[ 'post', 'http://example.org/wp-admin/post-new.php',                'http://example.org/wp-admin/post.php?post=2&amp;action=edit' ],
+			[ 'page', 'http://example.org/wp-admin/post-new.php?post_type=page', 'http://example.org/wp-admin/post.php?post=2&amp;action=edit' ]
+		];
+	}
 
-		$this->assertInstanceOf( MslsAdminIcon::class, $obj->set_path() );
-		$this->assertInstanceOf( MslsAdminIcon::class, $obj->set_language( 'de_DE' ) );
-		$this->assertInstanceOf( MslsAdminIcon::class, $obj->set_src( '/dev/german_flag.png' ) );
+	function test_get_a_not_empty_post() {
+		list ( $post_type, $create_link, $edit_link ) = $this->get_post_type()[0];
 
-		$this->assertEquals( '<img alt="de_DE" src="/dev/german_flag.png" />', $obj->get_img() );
-		$this->assertInternalType( 'string', $obj->get_edit_new() );
+		Functions\expect( 'get_edit_post_link' )->once()->andReturn( $edit_link );
 
-		$this->assertInstanceOf( MslsAdminIcon::class, $obj->set_href( $post->ID ) );
+		$post = $this->get_post( $post_type, 2 );
+		$obj  = $this->get_test( $post );
 
-		$value = '<a title="Edit the translation in the de_DE-blog" href="http://example.org/wp-admin/post.php?post=' . $post->ID . '&amp;action=edit"><img alt="de_DE" src="/dev/german_flag.png" /></a>&nbsp;';
+		$value = '<a title="Edit the translation in the de_DE-blog" href="' . $edit_link . '"><img alt="de_DE" src="/dev/german_flag.png" /></a>&nbsp;';
 		$this->assertEquals( $value, $obj->get_a() );
 		$this->assertEquals( $value, $obj->__toString() );
+	}
+
+	function test_get_a_not_empty_page() {
+		list ( $post_type, $create_link, $edit_link ) = $this->get_post_type()[1];
+
+		Functions\expect( 'add_query_arg' )->twice()->andReturn( $edit_link );
+		Functions\expect( 'get_edit_post_link' )->once()->andReturn( $edit_link );
+
+		$post = $this->get_post( $post_type, 2 );
+		$obj  = $this->get_test( $post );
+
+		$value = '<a title="Edit the translation in the de_DE-blog" href="' . $edit_link . '"><img alt="de_DE" src="/dev/german_flag.png" /></a>&nbsp;';
+		$this->assertEquals( $value, $obj->get_a() );
+		$this->assertEquals( $value, $obj->__toString() );
+	}
+
+	public function test_get_a_empty_post() {
+		list ( $post_type, $create_link, $edit_link ) = $this->get_post_type()[0];
+
+		Functions\expect( 'get_current_blog_id' )->twice()->andReturn( 1 );
+		Functions\expect( 'get_admin_url' )->twice()->andReturn( $create_link );
+		Functions\expect( 'get_edit_post_link' )->twice()->andReturn( null );
+
+		$post = $this->get_post( $post_type );
+		$obj  = $this->get_test( $post );
 
 		$this->assertInstanceOf( MslsAdminIcon::class, $obj->set_href( 0 ) );
 
@@ -57,18 +85,64 @@ class WP_Test_MslsAdminIcon extends Msls_UnitTestCase {
 		$this->assertEquals( $value, $obj->__toString() );
 	}
 
+	public function test_get_a_empty_page() {
+		list ( $post_type, $create_link, $edit_link ) = $this->get_post_type()[1];
+
+		Functions\expect( 'get_current_blog_id' )->twice()->andReturn( 1 );
+		Functions\expect( 'get_admin_url' )->twice()->andReturn( $create_link );
+		Functions\expect( 'add_query_arg' )->twice()->andReturn( $create_link );
+		Functions\expect( 'get_edit_post_link' )->twice()->andReturn( null );
+
+		$post = $this->get_post( $post_type );
+		$obj  = $this->get_test( $post );
+
+		$this->assertInstanceOf( MslsAdminIcon::class, $obj->set_href( 0 ) );
+
+		$value = sprintf( '<a title="Create a new translation in the de_DE-blog" href="%s"><img alt="de_DE" src="/dev/german_flag.png" /></a>&nbsp;', $create_link );
+		$this->assertEquals( $value, $obj->get_a() );
+		$this->assertEquals( $value, $obj->__toString() );
+	}
+
+	public function test_get_img_post() {
+		list ( $post_type, $create_link, $edit_link ) = $this->get_post_type()[0];
+
+		Functions\expect( 'get_current_blog_id' )->once()->andReturn( 1 );
+		Functions\expect( 'get_admin_url' )->once()->andReturn( $this->admin_url );
+		Functions\expect( 'get_edit_post_link' )->once()->andReturn( $edit_link );
+
+		$post = $this->get_post( $post_type );
+		$obj  = $this->get_test( $post );
+
+		$this->assertEquals( '<img alt="de_DE" src="/dev/german_flag.png" />', $obj->get_img() );
+		$this->assertInternalType( 'string', $obj->get_edit_new() );
+	}
+
+
+	public function test_get_img_post_page() {
+		list ( $post_type, $create_link, $edit_link ) = $this->get_post_type()[1];
+
+		Functions\expect( 'add_query_arg' )->twice()->andReturn( $edit_link );
+		Functions\expect( 'get_current_blog_id' )->once()->andReturn( 1 );
+		Functions\expect( 'get_admin_url' )->once()->andReturn( $this->admin_url );
+		Functions\expect( 'get_edit_post_link' )->once()->andReturn( $edit_link );
+
+		$post = $this->get_post( $post_type );
+		$obj  = $this->get_test( $post );
+
+		$this->assertEquals( '<img alt="de_DE" src="/dev/german_flag.png" />', $obj->get_img() );
+		$this->assertInternalType( 'string', $obj->get_edit_new() );
+	}
+
 	public function test_path_is_built_using_id_and_language_if_available() {
-		Functions\when( 'add_query_arg' )->returnArg();
+		list ( $post_type, $create_link, $edit_link ) = $this->get_post_type()[0];
 
-		$post = \Mockery::mock( '\WP_Post' );
-		$post->ID = 2;
-		$post->post_type = 'post';
-		$post->post_author = 'realloc';
+		Functions\expect( 'add_query_arg' )->once()->andReturn( 'abc' );
+		Functions\expect( 'get_current_blog_id' )->once()->andReturn( 1 );
+		Functions\expect( 'get_admin_url' )->once()->andReturn( $this->admin_url );
+		Functions\expect( 'get_edit_post_link' )->once()->andReturn( 'def' );
 
-		$obj = new MslsAdminIcon( 'post' );
-		$lang = 'de_DE';
-		$obj->set_origin_language( $lang );
-		$obj->set_id( $post );
+		$post = $this->get_post( $post_type );
+		$obj  = $this->get_test( $post );
 
 		$a = $obj->get_edit_new();
 
