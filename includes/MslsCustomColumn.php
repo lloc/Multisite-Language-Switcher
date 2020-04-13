0676d1ccd0dca4f08e60b882612f4e2f47a303ae
abed23 <hdzieciol@gmail.com>
2020-04-13 11:54:09 +0100

    Sortable language columns - incomplete
    I splited translated languages into separate columns and added sortable property to make it easier to work with bulk content. However, I am not a pro-php programmer and haven't managed to implement the sorting query correctly. Everything else is done though. There are also some adjustments here for the issue with translation linking described in #147.

--- includes/MslsCustomColumn.php
+++ includes/MslsCustomColumn.php
@@ -28,18 +28,48 @@ class MslsCustomColumn extends MslsMain {
 
 		if ( ! $options->is_excluded() ) {
 			$post_type = MslsPostType::instance()->get_request();
-			
 			if ( ! empty( $post_type ) ) {
 				add_filter( "manage_{$post_type}_posts_columns", [ $obj, 'th' ] );
 				add_action( "manage_{$post_type}_posts_custom_column", [ $obj, 'td' ], 10, 2 );
 				add_action( "manage_edit-{$post_type}_sortable_columns", [ $obj, 'sortable_cols' ], 10, 2 );
-				
+				add_action( "pre_get_{$post_type}", [$obj, "orderby_translation"]);
 				add_action( 'trashed_post', [ $obj, 'delete' ] );
 			}
 		}
 
 		return $obj;
 	}
+	
+	/**
+	* Making stuff sortable 
+	* @param WP_Query $query
+	*/
+	public function orderby_translation ($query) {
+		
+		$blogs = $this->collection->get();
+		$orderby = $query->get('orderby');
+		if ($blogs) {
+			// Scan data for matching language 
+			foreach ( $blogs as $blog ) {
+				$language = $blog->get_language();
+				$col = 'mslcol_' . $language;
+				if (strcmp($col, $orderby) == 0) {
+					//print_r('Matching language - not sure how to check existing language versions from here');
+				}
+			}
+			
+		}
+		
+		$origin_language = MslsBlogCollection::get_blog_language();
+
+		if( ! is_admin() || ! $query->is_main_query() ) {
+			return;
+		}
+
+		// Not sure how to go from here (limited dbaccess to look into records)
+	  
+	}
+
 	
 	/**
 	* Table header- Sorting
@@ -82,11 +112,9 @@ class MslsCustomColumn extends MslsMain {
 					$icon->set_origin_language( 'it_IT' );
 				}
 
-				/* $arr[] = $icon->get_icon(); */
 				$sep_colname = 'mslcol_' . $blog->get_language();
 				$columns[$sep_colname] =  $icon->get_icon();
 			}
-			/* $columns['mslscol'] = implode( '&nbsp;', $arr ); */
 		}
 
 		return $columns;
@@ -105,13 +133,55 @@ class MslsCustomColumn extends MslsMain {
 		// Check for msl column name
 		$msl_pos = strpos($column_name, 'mslcol_');
 		if ( $msl_pos == 0 ) {
+			// Get all blogs
 			$blogs           = $this->collection->get();
 			$origin_language = MslsBlogCollection::get_blog_language();
+			
+			// Set original source
+			$ids[]			 = $item_id;
+			$langs[]		 = $origin_language;
+			
 			// Filter out the language 
 			$columns_language = substr($column_name, strlen('mslcol_'), strlen($column_name));
 			// print_r($columns_language);
 			if ( $blogs ) {
+				
+				// Get interlinking between translations 
 				$mydata = MslsOptions::create( $item_id );
+				foreach( $blogs as $blog) {
+					switch_to_blog( $blog->userblog_id );
+					$lang = $blog->get_language();
+					
+					// Set as nothing
+					$obj_id = -1; 
+					
+					// Handle Terms
+					if ($mydata instanceof MslsOptionsTaxTerm){
+						
+						$temp = get_term( $mydata->$lang, $mydata->base  );
+					
+						if (!is_wp_error( $temp )){ 
+							$obj_id = $temp->term_id;
+						}
+					}
+					
+					// Handle Posts
+					if ($mydata instanceof MslsOptionsPost){
+						$temp = get_post( $mydata->$lang );
+						if (!is_wp_error( $temp )){ 
+							$obj_id = $temp->ID;
+						}
+					}
+
+					// Do not store empty records 
+					if ( strcmp($blog->get_language(), $origin_language) != 0 && $obj_id != $item_id) { 
+						$ids[] = $obj_id;
+						$langs[] = $blog->get_language();
+					}
+					restore_current_blog();
+				}
+				
+				// Print icons with changed links 
 				foreach ( $blogs as $blog ) {
 					switch_to_blog( $blog->userblog_id );
 
@@ -119,8 +189,8 @@ class MslsCustomColumn extends MslsMain {
 
 					$icon = MslsAdminIcon::create();
 					$icon->set_language( $language );
-					$icon->set_id( $item_id );
-					$icon->set_origin_language( $origin_language );
+					$icon->set_id( $ids );
+					$icon->set_origin_language( $langs );
 
 					$icon->set_icon_type( 'action' );
 
@@ -136,6 +206,7 @@ class MslsCustomColumn extends MslsMain {
 
 					restore_current_blog();
 				}
+				
 			}
 		}
 	}
