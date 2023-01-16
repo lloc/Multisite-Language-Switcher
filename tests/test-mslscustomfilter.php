5695b1c85eeca41a9a5757858765e32e8a8750c9
Dennis Ploetner <re@lloc.de>
2023-01-16 11:55:18 +0100

    restoreAll() applied

--- tests/test-mslscustomfilter.php
+++ tests/test-mslscustomfilter.php
@@ -7,15 +7,20 @@ use lloc\Msls\MslsCustomFilter;
 use lloc\Msls\MslsOptions;
 use function Patchwork\always;
 use function Patchwork\redefine;
+use function Patchwork\restoreAll;
 
 class WP_Test_MslsCustomFilter extends Msls_UnitTestCase {
 
 	public function setUp(): void {
 		parent::setUp();
 
-		redefine( 'filter_has_var', always( true ) );
 		redefine( 'filter_input', always( 1 ) );
+	}
 
+	public function tearDown(): void {
+		parent::tearDown();
+
+		restoreAll();
 	}
 
 	public function get_sut( $blogs = [] ): MslsCustomFilter {
@@ -27,7 +32,9 @@ class WP_Test_MslsCustomFilter extends Msls_UnitTestCase {
 		return new MslsCustomFilter( $options, $collection );
 	}
 
-	public function test_add_filter_no_blogs() {
+	public function test_add_filter_has_var_false() {
+		redefine( 'filter_has_var', always( false ) );
+
 		$sut = $this->get_sut();
 
 		$this->expectOutputString( '' );
@@ -35,15 +42,29 @@ class WP_Test_MslsCustomFilter extends Msls_UnitTestCase {
 		$sut->add_filter();
 	}
 
-	public function test_add_filter_with_blogs() {
+	public function test_add_filter() {
+		redefine( 'filter_has_var', always( true ) );
+
 		$sut = $this->get_sut();
 
 		$this->expectOutputString( '' );
 
 		$sut->add_filter();
+	}
+
+	function test_execute_filter_has_var_false() {
+		redefine( 'filter_has_var', always( false ) );
+
+		$sut = $this->get_sut();
+
+		$query = \Mockery::mock( 'WP_Query' );
+
+		$this->assertFalse( $sut->execute_filter( $query ) );
 	}
 
 	function test_execute_filter() {
+		redefine( 'filter_has_var', always( true ) );
+
 		$sut = $this->get_sut();
 
 		$query = \Mockery::mock( 'WP_Query' );
