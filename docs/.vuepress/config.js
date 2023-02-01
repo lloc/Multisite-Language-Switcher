980af6c9cf4237815bbb52f5bd85ee9f3699d7b9
Dennis Ploetner <re@lloc.de>
2023-02-01 16:36:03 +0100

    Vuepress config updated

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,7 +23,7 @@ module.exports = {
       },
       {
         text: 'API documentation',
-        link: '/api-documentation/',
+        link: 'http://msls.co/api-documentation/',
       }
     ],
     sidebar: [
