c57f0fdfc0c5b81d4d88c77f670dd3dad11d7fff
Dennis Ploetner <re@lloc.de>
2023-02-02 12:57:07 +0100

    Documentation task added

--- bin/documentation.sh
+++ bin/documentation.sh
@@ -7,10 +7,4 @@ docker run --rm -v "$(pwd):/data" "phpdoc/phpdoc:3"
 
 cd docs/.vuepress/dist
 echo 'msls.co' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:lloc/Multisite-Language-Switcher.git master:gh-pages
-
 cd -
