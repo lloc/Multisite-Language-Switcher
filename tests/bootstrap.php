1276f7c3ecefbaf002501113b7234064700d83b2
Dennis Ploetner <re@lloc.de>
2023-08-07 16:57:06 +0200

    Test factory for BlogCollection

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,8 @@
 
 namespace lloc\MslsTests;
 
+use lloc\Msls\MslsBlog;
+use lloc\Msls\MslsBlogCollection;
 use PHPUnit\Framework\TestCase;
 use Brain\Monkey;
 use Brain\Monkey\Functions;
@@ -23,6 +25,31 @@ class Msls_UnitTestCase extends TestCase {
 		Functions\when( 'esc_attr' )->returnArg();
 		Functions\when( 'esc_url' )->returnArg();
 		Functions\when( '__' )->returnArg();
+	}
+
+	/**
+	 * Get a of the MslsBlogCollection class that contains some blogs
+	 *
+	 * @param $map
+	 *
+	 * @return void
+	 */
+	public function getBlogsCollection( $map = [ 'de_DE' => 'de', 'en_US' => 'en' ] ): MslsBlogCollection {
+		foreach ( $map as $locale => $alpha2 ) {
+			$blog = \Mockery::mock( MslsBlog::class );
+			$blog->shouldReceive( [
+				'get_alpha2'   => $alpha2,
+				'get_language' => $locale,
+			] );
+
+			$blogs[] = $blog;
+		}
+
+		$collection = \Mockery::mock( MslsBlogCollection::class );
+		$collection->shouldReceive( 'get_objects' )->andReturn( $blogs );
+		$collection->shouldReceive( 'get' )->andReturn( $blogs );
+
+		return $collection;
 	}
 
 	protected function tearDown(): void {
--- tests/test-mslscustomcolumn.php
+++ tests/test-mslscustomcolumn.php
@@ -5,10 +5,23 @@ namespace lloc\MslsTests;
 use lloc\Msls\MslsCustomColumn;
 use lloc\Msls\MslsOptions;
 use lloc\Msls\MslsBlogCollection;
+use Brain\Monkey\Functions;
 
 class WP_Test_MslsCustomColumn extends Msls_UnitTestCase {
 
 	function test_th() {
+		Functions\expect( 'add_query_arg' )->twice()->andReturn( 'https://example.org/added-args' );
+		Functions\expect( 'get_the_ID' )->twice()->andReturnValues( [ 1, 2 ] );
+		Functions\when( 'plugin_dir_path' )->justReturn( dirname( __DIR__, 1 ) . '/' );
+
+		$options = \Mockery::mock( MslsOptions::class );
+		$obj     = new MslsCustomColumn( $options, $this->getBlogsCollection() );
+
+		$expected = [ 'mslscol' => '<span class="flag-icon flag-icon-de">de_DE</span>&nbsp;<span class="flag-icon flag-icon-us">en_US</span>' ];
+		$this->assertEquals( $expected, $obj->th( [] ) );
+	}
+
+	function test_th_empty() {
 		$options    = \Mockery::mock( MslsOptions::class );
 		$collection = \Mockery::mock( MslsBlogCollection::class );
 		$collection->shouldReceive( 'get' )->once()->andReturn( [] );
