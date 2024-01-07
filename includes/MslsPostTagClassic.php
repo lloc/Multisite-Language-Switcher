23329c146f543b018a28e31aa6ecfff22aff690a
Dennis Ploetner <re@lloc.de>
2024-01-07 15:04:02 +0100

    Static counter against double output

--- includes/MslsPostTagClassic.php
+++ includes/MslsPostTagClassic.php
@@ -20,10 +20,6 @@ class MslsPostTagClassic extends MslsPostTag {
 	 * @param string $taxonomy
 	 */
 	public function add_input( string $taxonomy ): void {
-		if ( did_action( "{$taxonomy}_add_form_fields" ) !== 1 ) {
-			return;
-		}
-
 		$title_format = '<h3>%s</h3>';
 
 		$item_format = '<label for="msls_input_%1$s">%2$s</label>
@@ -44,10 +40,6 @@ class MslsPostTagClassic extends MslsPostTag {
 	 * @param string $taxonomy
 	 */
 	public function edit_input( \WP_Term $tag, string $taxonomy ): void {
-		if ( did_action( "{$taxonomy}_edit_form_fields" ) !== 1 ) {
-			return;
-		}
-
 		$title_format = '<tr>
 			<th colspan="2">
 			<strong>%s</strong>
@@ -64,13 +56,51 @@ class MslsPostTagClassic extends MslsPostTag {
 			</select></td>
 			</tr>';
 
-		$this->the_input( $taxonomy, $title_format, $item_format );
+		$this->the_input( $tag, $title_format, $item_format );
+	}
+
+	/**
+	 * Print the input fields
+	 * Returns true if the blogcollection is not empty
+	 *
+	 * @param ?\WP_Term $tag
+	 * @param string $title_format
+	 * @param string $item_format
+	 *
+	 * @return boolean
+	 */
+	public function the_input( ?\WP_Term $tag, string $title_format, string $item_format ): bool {
+		static $count = 0;
+
+		if ( $count > 0 ) {
+			return false;
+		}
+
+		$count ++;
+
+		$blogs = $this->collection->get();
+		if ( ! empty( $blogs ) ) {
+			$term_id = $tag->term_id ?? 0;
+			$mydata  = MslsOptionsTax::create( $term_id );
+			$type    = MslsContentTypes::create()->get_request();
+
+			$this->maybe_set_linked_term( $mydata );
+
+			printf( $title_format, $this->get_select_title() );
+
+			foreach ( $blogs as $blog ) {
+				$this->print_option( $blog, $type, $mydata, $item_format );
+			}
+
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
 	 * Prints options inputs
 	 *
-	 * @uses selected
 	 * @param MslsBlog $blog
 	 * @param string $type
 	 * @param MslsOptionsTax $mydata
@@ -87,8 +117,8 @@ class MslsPostTagClassic extends MslsPostTag {
 			$icon->set_href( $mydata->$language );
 		}
 
-		$options  = '';
-		$terms    = get_terms( [ 'taxonomy' => $type, 'hide_empty' => false ] );
+		$options = '';
+		$terms   = get_terms( [ 'taxonomy' => $type, 'hide_empty' => false ] );
 		if ( ! empty( $terms ) ) {
 			foreach ( $terms as $term ) {
 				$options .= sprintf(
@@ -96,6 +126,7 @@ class MslsPostTagClassic extends MslsPostTag {
 					$term->term_id,
 					selected( $term->term_id, $mydata->$language, false ),
 					$term->name
+
 				);
 			}
 		}
@@ -105,40 +136,4 @@ class MslsPostTagClassic extends MslsPostTag {
 		restore_current_blog();
 	}
 
-	/**
-	 * Print the input fields
-	 * Returns true if the blogcollection is not empty
-	 *
-	 * @param mixed $tag
-	 * @param string $title_format
-	 * @param string $item_format
-	 * @return boolean
-	 */
-	public function the_input( $tag, $title_format, $item_format ) {
-		$blogs = $this->collection->get();
-
-		if ( ! empty( $blogs ) ) {
-			$term_id = is_object( $tag ) ? $tag->term_id : 0;
-			$mydata  = MslsOptionsTax::create( $term_id );
-			$type    = MslsContentTypes::create()->get_request();
-
-			$this->maybe_set_linked_term( $mydata );
-
-			printf(
-				$title_format,
-				apply_filters(
-					'msls_term_select_title',
-					__( 'Multisite Language Switcher', 'multisite-language-switcher' )
-				)
-			);
-
-			foreach ( $blogs as $blog ) {
-				$this->print_option( $blog, $type, $mydata, $item_format );
-			}
-
-			return true;
-		}
-
-		return false;
-	}
 }
