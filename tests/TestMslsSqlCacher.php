2db04afa9d24d3890f2cc76a7ecec1c5bb9a62d1
Dennis Ploetner <re@lloc.de>
2024-05-01 13:18:58 +0200

    Chore

--- tests/TestMslsSqlCacher.php
+++ tests/TestMslsSqlCacher.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare( strict_types=1 );
 
 namespace lloc\MslsTests;
 
@@ -7,37 +7,35 @@ use lloc\Msls\MslsSqlCacher;
 
 class TestMslsSqlCacher extends MslsUnitTestCase {
 
-	public function get_sut(): MslsSqlCacher {
+	protected function setUp(): void {
 		$wpdb = \Mockery::mock( \WPDB::class );
 		$wpdb->shouldReceive( [
 			'prepare'     => '',
 			'get_results' => []
 		] );
 
-		return new MslsSqlCacher( $wpdb, 'MslsSqlCacherTest' );
+		$this->test = new MslsSqlCacher( $wpdb, 'MslsSqlCacherTest' );
 	}
 
 	public function test_set_params_method(): void {
 		Functions\when( 'wp_cache_get' )->justReturn( false );
 		Functions\when( 'wp_cache_set' )->justReturn( true );
 
-		$obj = $this->get_sut();
+		$this->assertInstanceOf( MslsSqlCacher::class, $this->test->set_params( array( 'Cache', 'Test' ) ) );
+		$this->assertIsSTring( $this->test->get_key() );
+		$this->assertEquals( 'MslsSqlCacherTest_Cache_Test', $this->test->get_key() );
 
-		$this->assertInstanceOf( MslsSqlCacher::class, $obj->set_params( array( 'Cache', 'Test' ) ) );
-		$this->assertIsSTring( $obj->get_key() );
-		$this->assertEquals( 'MslsSqlCacherTest_Cache_Test', $obj->get_key() );
+		$this->assertInstanceOf( MslsSqlCacher::class, $this->test->set_params( 'Cache_Test' ) );
+		$this->assertIsSTring( $this->test->get_key() );
+		$this->assertEquals( 'MslsSqlCacherTest_Cache_Test', $this->test->get_key() );
 
-		$this->assertInstanceOf( MslsSqlCacher::class, $obj->set_params( 'Cache_Test' ) );
-		$this->assertIsSTring( $obj->get_key() );
-		$this->assertEquals( 'MslsSqlCacherTest_Cache_Test', $obj->get_key() );
-
-		$sql = $obj->prepare(
-			"SELECT blog_id FROM {$obj->blogs} WHERE blog_id != %d AND site_id = %d",
-			$obj->blogid,
-			$obj->siteid
+		$sql = $this->test->prepare(
+			"SELECT blog_id FROM {$this->test->blogs} WHERE blog_id != %d AND site_id = %d",
+			$this->test->blogid,
+			$this->test->siteid
 		);
 		$this->assertIsSTring( $sql );
-		$this->assertIsArray( $obj->get_results( $sql ) );
+		$this->assertIsArray( $this->test->get_results( $sql ) );
 	}
 
 }
