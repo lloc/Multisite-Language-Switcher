49f04f139025ae66afec2ae6803dc1e5a62d3a9a
Dennis Ploetner <re@lloc.de>
2024-05-18 10:55:14 +0200

    Compiled block added

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '68612a682314c0702bd7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '627848d7182ac425e62c');
