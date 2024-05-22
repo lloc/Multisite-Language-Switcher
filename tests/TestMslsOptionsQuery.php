de96af61ede194c3f74f71e49371c61064989d24
Dennis Ploetner <re@lloc.de>
2024-05-22 23:33:36 +0200

    Unit tests added

--- tests/TestMslsOptionsQuery.php
+++ tests/TestMslsOptionsQuery.php
@@ -5,13 +5,67 @@ namespace lloc\MslsTests;
 use Brain\Monkey\Functions;
 
 use lloc\Msls\MslsOptionsQuery;
+use lloc\Msls\MslsOptionsQueryAuthor;
+use lloc\Msls\MslsOptionsQueryDay;
+use lloc\Msls\MslsOptionsQueryMonth;
+use lloc\Msls\MslsOptionsQueryPostType;
+use lloc\Msls\MslsOptionsQueryYear;
 
 /**
  * TestMslsOptionsQuery
  */
 class TestMslsOptionsQuery extends MslsUnitTestCase {
 
-	public function test_create(): void {
+	public function test_create_is_day(): void {
+		Functions\expect( 'is_day' )->once()->andReturn( true );
+		Functions\expect( 'get_query_var' )->times( 3 )->andReturnValues( array( 1969, 6, 26 ) );
+		Functions\expect( 'get_option' )->once();
+
+		$this->assertInstanceOf( MslsOptionsQueryDay::class, MslsOptionsQuery::create() );
+	}
+	public function test_create_is_month(): void {
+		Functions\expect( 'is_day' )->once()->andReturn( false );
+		Functions\expect( 'is_month' )->once()->andReturn( true );
+		Functions\expect( 'get_query_var' )->twice()->andReturnValues( array( 1969, 6 ) );
+		Functions\expect( 'get_option' )->once();
+
+		$this->assertInstanceOf( MslsOptionsQueryMonth::class, MslsOptionsQuery::create() );
+	}
+
+	public function test_create_is_year(): void {
+		Functions\expect( 'is_day' )->once()->andReturn( false );
+		Functions\expect( 'is_month' )->once()->andReturn( false );
+		Functions\expect( 'is_year' )->once()->andReturn( true );
+		Functions\expect( 'get_query_var' )->once()->andReturn( 1969 );
+		Functions\expect( 'get_option' )->once();
+
+		$this->assertInstanceOf( MslsOptionsQueryYear::class, MslsOptionsQuery::create() );
+	}
+
+	public function test_create_is_author(): void {
+		Functions\expect( 'is_day' )->once()->andReturn( false );
+		Functions\expect( 'is_month' )->once()->andReturn( false );
+		Functions\expect( 'is_year' )->once()->andReturn( false );
+		Functions\expect( 'is_author' )->once()->andReturn( true );
+		Functions\expect( 'get_queried_object_id' )->once()->andReturn( 42 );
+		Functions\expect( 'get_option' )->once();
+
+		$this->assertInstanceOf( MslsOptionsQueryAuthor::class, MslsOptionsQuery::create() );
+	}
+
+	public function test_create_is_post_type_archive(): void {
+		Functions\expect( 'is_day' )->once()->andReturn( false );
+		Functions\expect( 'is_month' )->once()->andReturn( false );
+		Functions\expect( 'is_year' )->once()->andReturn( false );
+		Functions\expect( 'is_author' )->once()->andReturn( false );
+		Functions\expect( 'is_post_type_archive' )->once()->andReturn( true );
+		Functions\expect( 'get_query_var' )->once()->andReturn( 'book' );
+		Functions\expect( 'get_option' )->once();
+
+		$this->assertInstanceOf( MslsOptionsQueryPostType::class, MslsOptionsQuery::create() );
+	}
+
+	public function test_create_is_null(): void {
 		Functions\expect( 'is_day' )->once()->andReturn( false );
 		Functions\expect( 'is_month' )->once()->andReturn( false );
 		Functions\expect( 'is_year' )->once()->andReturn( false );
@@ -24,16 +78,15 @@ class TestMslsOptionsQuery extends MslsUnitTestCase {
 	public function test_current_get_postlink(): void {
 		$home_url = 'https://example.org/';
 
-		Functions\expect( 'get_option' )->once()->andReturn( [ 'de_DE' => 42 ] );
+		Functions\expect( 'get_option' )->once()->andReturn( array( 'de_DE' => 42 ) );
 		Functions\expect( 'home_url' )->once()->andReturn( $home_url );
 
 		$this->assertEquals( $home_url, ( new MslsOptionsQuery() )->get_postlink( 'de_DE' ) );
 	}
 
 	public function test_non_existent_get_postlink(): void {
-		Functions\expect( 'get_option' )->once()->andReturn( [ 'de_DE' => 42 ] );
+		Functions\expect( 'get_option' )->once()->andReturn( array( 'de_DE' => 42 ) );
 
 		$this->assertEquals( '', ( new MslsOptionsQuery() )->get_postlink( 'fr_FR' ) );
 	}
-
 }
--- tests/TestMslsOptionsQueryPostType.php
+++ tests/TestMslsOptionsQueryPostType.php
@@ -14,13 +14,20 @@ class TestMslsOptionsQueryPostType extends MslsUnitTestCase {
 	protected function setUp(): void {
 		parent::setUp();
 
-		Functions\expect( 'get_option' )->once()->andReturn( [ 'de_DE' => 42 ] );
+		Functions\expect( 'get_option' )->once()->andReturn( array( 'de_DE' => 42 ) );
 
 		$this->test = new MslsOptionsQueryPostType();
 	}
 
-	public function test_has_value(): void {
-		$this->assertIsBool( $this->test->has_value( 'de_DE' ) );
+	public function test_has_value_existing(): void {
+		$this->assertTrue( $this->test->has_value( 'de_DE' ) );
+	}
+
+	public function test_has_value_not_existing(): void {
+		$post_type = \Mockery::mock( '\WP_Post_Type' );
+		Functions\expect( 'get_post_type_object' )->once()->andReturn( $post_type );
+
+		$this->assertTrue( $this->test->has_value( 'it_IT' ) );
 	}
 
 	public function test_get_current_link(): void {
@@ -28,5 +35,4 @@ class TestMslsOptionsQueryPostType extends MslsUnitTestCase {
 
 		$this->assertEquals( 'https://example.org/queried-posttype', $this->test->get_current_link() );
 	}
-
 }
