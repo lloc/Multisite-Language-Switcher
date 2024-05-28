f079a00b6d57006b257fc091a560966a72eb6577
Dennis Ploetner <re@lloc.de>
2024-05-28 09:39:40 +0200

    get_alternate_links tested

--- tests/TestMslsOutput.php
+++ tests/TestMslsOutput.php
@@ -7,7 +7,9 @@ use lloc\Msls\MslsBlogCollection;
 use lloc\Msls\MslsOutput;
 use lloc\Msls\MslsOptions;
 use lloc\Msls\MslsOptionsPost;
+
 use Brain\Monkey\Functions;
+use Brain\Monkey\Filters;
 
 class TestMslsOutput extends MslsUnitTestCase {
 
@@ -26,21 +28,113 @@ class TestMslsOutput extends MslsUnitTestCase {
 		$this->assertEquals( array(), $this->test->get( 0 ) );
 	}
 
-	public function test_get_alternate_links() {
-		$blog = \Mockery::mock( MslsBlog::class );
-		$blog->shouldReceive( 'get_alpha2' )->andReturn( 'de' );
-		$blog->shouldReceive( 'get_language' )->andReturn( 'de_DE' );
+	public function test_get_alternate_links_two_url() {
+		$blogs = array();
+
+		$a = \Mockery::mock( MslsBlog::class );
+		$a->shouldReceive( 'get_alpha2' )->andReturn( 'de' );
+		$a->shouldReceive( 'get_language' )->andReturn( 'de_DE' );
+		$a->shouldReceive( 'get_url' )->andReturn( 'https://example.de/' );
+		$a->shouldReceive( 'get_description' )->andReturn( 'Deutsch' );
+
+		$blogs[] = $a;
+
+		$b = \Mockery::mock( MslsBlog::class );
+		$b->shouldReceive( 'get_alpha2' )->andReturn( 'it' );
+		$b->shouldReceive( 'get_language' )->andReturn( 'it_IT' );
+		$b->shouldReceive( 'get_url' )->andReturn( 'https://example.it/' );
+		$b->shouldReceive( 'get_description' )->andReturn( 'Italiano' );
+
+		$blogs[] = $b;
 
 		$collection = \Mockery::mock( MslsBlogCollection::class );
-		$collection->shouldReceive( 'get_objects' )->andReturn( array( $blog ) );
+		$collection->shouldReceive( 'get_objects' )->andReturn( $blogs );
 
 		Functions\expect( 'msls_blog_collection' )->once()->andReturn( $collection );
 		Functions\expect( 'is_admin' )->once()->andReturn( false );
 		Functions\expect( 'is_front_page' )->once()->andReturn( false );
 		Functions\expect( 'is_search' )->once()->andReturn( false );
 		Functions\expect( 'is_404' )->once()->andReturn( false );
+		Functions\expect( 'is_category' )->once()->andReturn( false );
+		Functions\expect( 'is_tag' )->once()->andReturn( false );
+		Functions\expect( 'is_tax' )->once()->andReturn( false );
+		Functions\expect( 'is_date' )->once()->andReturn( false );
+		Functions\expect( 'is_author' )->once()->andReturn( false );
+		Functions\expect( 'is_post_type_archive' )->once()->andReturn( false );
+		Functions\expect( 'get_queried_object_id' )->once()->andReturn( 42 );
+		Functions\expect( 'get_option' )->once()->andReturn( array() );
+		Functions\expect( 'esc_attr' )->times( 3 )->andReturnFirstArg();
 
-		$this->assertEquals( array(), $this->test->get_alternate_links() );
+		Filters\expectApplied( 'mlsl_output_get_alternate_links_arr' )->once();
+
+		$expected =
+			'<link rel="alternate" hreflang="de" href="https://example.de/" title="Deutsch" />' . PHP_EOL .
+			'<link rel="alternate" hreflang="it" href="https://example.it/" title="Italiano" />';
+		$this->assertEquals( $expected, $this->test->get_alternate_links() );
+	}
+
+	public function test_get_alternate_links_null_url() {
+		$blogs = array();
+
+		$a = \Mockery::mock( MslsBlog::class );
+		$a->shouldReceive( 'get_alpha2' )->andReturn( 'de' );
+		$a->shouldReceive( 'get_language' )->andReturn( 'de_DE' );
+		$a->shouldReceive( 'get_url' )->andReturnNull();
+
+		$blogs[] = $a;
+
+		$collection = \Mockery::mock( MslsBlogCollection::class );
+		$collection->shouldReceive( 'get_objects' )->andReturn( $blogs );
+
+		Functions\expect( 'msls_blog_collection' )->once()->andReturn( $collection );
+		Functions\expect( 'is_admin' )->once()->andReturn( false );
+		Functions\expect( 'is_front_page' )->once()->andReturn( false );
+		Functions\expect( 'is_search' )->once()->andReturn( false );
+		Functions\expect( 'is_404' )->once()->andReturn( false );
+		Functions\expect( 'is_category' )->once()->andReturn( false );
+		Functions\expect( 'is_tag' )->once()->andReturn( false );
+		Functions\expect( 'is_tax' )->once()->andReturn( false );
+		Functions\expect( 'is_date' )->once()->andReturn( false );
+		Functions\expect( 'is_author' )->once()->andReturn( false );
+		Functions\expect( 'is_post_type_archive' )->once()->andReturn( false );
+		Functions\expect( 'get_queried_object_id' )->once()->andReturn( 42 );
+		Functions\expect( 'get_option' )->once()->andReturn( array() );
+
+		$this->assertEquals( '', $this->test->get_alternate_links() );
+	}
+
+	public function test_get_alternate_links_one_url() {
+		$blogs = array();
+
+		$a = \Mockery::mock( MslsBlog::class );
+		$a->shouldReceive( 'get_alpha2' )->andReturn( 'de' );
+		$a->shouldReceive( 'get_language' )->andReturn( 'de_DE' );
+		$a->shouldReceive( 'get_url' )->andReturn( 'https://example.de/' );
+		$a->shouldReceive( 'get_description' )->andReturn( 'Deutsch' );
+
+		$blogs[] = $a;
+
+		$collection = \Mockery::mock( MslsBlogCollection::class );
+		$collection->shouldReceive( 'get_objects' )->andReturn( $blogs );
+
+		Functions\expect( 'msls_blog_collection' )->once()->andReturn( $collection );
+		Functions\expect( 'is_admin' )->once()->andReturn( false );
+		Functions\expect( 'is_front_page' )->once()->andReturn( false );
+		Functions\expect( 'is_search' )->once()->andReturn( false );
+		Functions\expect( 'is_404' )->once()->andReturn( false );
+		Functions\expect( 'is_category' )->once()->andReturn( false );
+		Functions\expect( 'is_tag' )->once()->andReturn( false );
+		Functions\expect( 'is_tax' )->once()->andReturn( false );
+		Functions\expect( 'is_date' )->once()->andReturn( false );
+		Functions\expect( 'is_author' )->once()->andReturn( false );
+		Functions\expect( 'is_post_type_archive' )->once()->andReturn( false );
+		Functions\expect( 'get_queried_object_id' )->once()->andReturn( 42 );
+		Functions\expect( 'get_option' )->once()->andReturn( array() );
+		Functions\expect( 'esc_attr' )->twice()->andReturnFirstArg();
+
+		Filters\expectApplied( 'mlsl_output_get_alternate_links_default' )->once();
+
+		$this->assertEquals( '<link rel="alternate" hreflang="x-default" href="https://example.de/" title="Deutsch" />', $this->test->get_alternate_links() );
 	}
 
 	public function test___toString() {
