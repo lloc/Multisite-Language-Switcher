9928386705edcea56350738a288ec538ce570108
Dennis Ploetner <re@lloc.de>
2024-06-05 09:29:08 +0200

    MslsContentFilter refatored from MslsPlugin

--- includes/MslsPlugin.php
+++ includes/MslsPlugin.php
@@ -49,11 +49,11 @@ class MslsPlugin {
 			add_action( 'init', array( MslsAdminBar::class, 'init' ) );
 			add_action( 'init', array( MslsBlock::class, 'init' ) );
 			add_action( 'init', array( MslsShortCode::class, 'init' ) );
+			add_action( 'init', array( MslsContentFilter::class, 'init' ) );
 			add_action( 'widgets_init', array( MslsWidget::class, 'init' ) );
 			add_action( 'wp_head', array( __CLASS__, 'print_alternate_links' ) );
 
 			add_filter( 'msls_get_output', array( __CLASS__, 'get_output' ) );
-			add_filter( 'the_content', array( $obj, 'content_filter' ) );
 
 			\lloc\Msls\ContentImport\Service::instance()->register();
 
@@ -125,70 +125,6 @@ class MslsPlugin {
 	 */
 	public static function print_alternate_links() {
 		echo self::get_output()->get_alternate_links(), PHP_EOL;
-	}
-
-	/**
-	 * Filter for the_content()
-	 *
-	 * @param string $content
-	 *
-	 * @return string
-	 */
-	public function content_filter( string $content ) {
-		if ( ! is_front_page() && is_singular() && $this->options->is_content_filter() ) {
-			$content .= $this->filter_string();
-		}
-
-		return $content;
-	}
-
-	/**
-	 * Create filter-string for msls_content_filter()
-	 *
-	 * @param string $pref
-	 * @param string $post
-	 *
-	 * @return string
-	 */
-	public function filter_string( $pref = '<p id="msls">', $post = '</p>' ) {
-		$obj   = MslsOutput::init();
-		$links = $obj->get( 1, true, true );
-
-		/* translators: %s: list of languages */
-		$output = __( 'This post is also available in %s.', 'multisite-language-switcher' );
-
-		if ( has_filter( 'msls_filter_string' ) ) {
-			/**
-			 * Overrides the string for the output of the translation hint
-			 *
-			 * @param string $output
-			 * @param array $links
-			 *
-			 * @since 1.0
-			 */
-			$output = apply_filters( 'msls_filter_string', $output, $links );
-		} elseif ( count( $links ) > 1 ) {
-				$last = array_pop( $links );
-
-				/* translators: %1$s: list of languages separated by a comma, %2$s: last language */
-				$format = __( '%1$s and %2$s', 'multisite-language-switcher' );
-
-				$output = sprintf(
-					$output,
-					sprintf(
-						$format,
-						implode( ', ', $links ),
-						$last
-					)
-				);
-		} elseif ( 1 == count( $links ) ) {
-			$output = sprintf(
-				$output,
-				$links[0]
-			);
-		}
-
-		return ! empty( $output ) ? $pref . $output . $post : '';
 	}
 
 	/**
