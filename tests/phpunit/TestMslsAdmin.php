203572e7363dc08965150b847a5cbf7ede06d484
jose-gonzalez-motork <139996590+jose-gonzalez-motork@users.noreply.github.com>
2025-12-29 17:12:44 +0100

    Update TestMslsAdmin.php
    Replace the number with the constant

--- tests/phpunit/TestMslsAdmin.php
+++ tests/phpunit/TestMslsAdmin.php
@@ -138,7 +138,10 @@ final class TestMslsAdmin extends MslsUnitTestCase {
 
 		$this->expectException( \Exception::class );
 		$this->expectExceptionMessage(
-			'Multisite Language Switcher: Collection for reference user has been truncated because it exceeded the maximum of 200 users. Please, use the hook "msls_reference_users" to filter the result before!'
+		    sprintf(
+		        'Multisite Language Switcher: Collection for reference user has been truncated because it exceeded the maximum of %d users. Please, use the hook "msls_reference_users" to filter the result before!',
+		        MslsAdmin::MAX_REFERENCE_USERS
+		    )
 		);
 
 		$obj->reference_user();
